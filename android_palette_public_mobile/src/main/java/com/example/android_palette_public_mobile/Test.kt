b18caf05305bf3020d11a31be2d71dc929a959d0
QuickChangeSpidey <akshay.pandey@telus.com>
2022-11-02 13:49:10 -0400

    example added

---
+++ android_palette_public_mobile/src/main/java/com/example/android_palette_public_mobile/Test.kt
@@ -0,0 +1,5 @@
+package com.example.android_palette_public_mobile
+
+object Sydney {
+    val suburbs = listOf("Ryde", "Chippendale")
+}
--- app/src/main/java/com/example/android_palette_publicmobile/MainActivity.kt
+++ app/src/main/java/com/example/android_palette_publicmobile/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.android_palette_publicmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_palette_public_mobile.Sydney
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println(Sydney.suburbs)
     }
 }
